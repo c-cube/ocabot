672666d346053f8dcf2b4434a080720db03bd24e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2018-09-18 23:22:23 -0500

    fix build

--- src/hello.ml
+++ src/hello.ml
@@ -3,7 +3,7 @@ open Calculon
 
 let cmd_hello : Command.t =
   Command.make_simple
-    ~descr:"politeness core interface" ~prefix:"hello" ~prio:10
+    ~descr:"politeness core interface" ~cmd:"hello" ~prio:10
     (fun (input_msg:Core.privmsg) _ ->
        let who = input_msg.Core.nick in
        Lwt.return (Some ("hello " ^ who))
