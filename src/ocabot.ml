e2b5d3c3cda11e189000fac694e82c73bd6fb0ba
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-06-13 22:00:22 -0400

    use sasl

--- src/ocabot.ml
+++ src/ocabot.ml
@@ -12,18 +12,24 @@ let all_ : C.Plugin.t list = [
 let config = {
   C.Config.default with
   C.Config.
-  server = "irc.freenode.net";
-  port = 7000;
+  log_level = Logs.Info;
+  server = "irc.libera.chat";
+  port = 6697;
   username = "ocabot";
   realname = "ocabot";
   nick = "ocabot";
   channel = "#ocaml";
+  tls = true;
+  sasl = true;
 }
 
 let () =
+  Logs.set_reporter (Logs.format_reporter ());
   try
     (* update with CLI parameters *)
     let config = C.Config.parse config Sys.argv in
+    Logs.set_level ~all:true (Some config.C.Config.log_level);
+    Logs.info (fun k->k"start ocabot");
     C.Run_main.main config all_ |> Lwt_main.run
   with
     | Arg.Help msg -> print_endline msg
